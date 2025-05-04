e2923582692963a2c78e6471c3dd0c28a92b1894
carlosziegler <carlos.ziegler@gmail.com>
2025-05-04 22:01:17 +0200

    fix: adjust VITE_SERVER_URL on auth

--- src/lib/auth/auth-client.ts
+++ src/lib/auth/auth-client.ts
@@ -10,6 +10,7 @@ import {
 import { createAuthClient } from "better-auth/react";
 
 export const authClient = createAuthClient({
+  baseURL: env.VITE_SERVER_URL,
   plugins: [
     twoFactorClient(),
     passkeyClient(),

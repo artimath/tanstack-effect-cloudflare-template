21255b10a84d4880fc609c4a4e3f395bbc8675bb
carlosziegler <carlos.ziegler@gmail.com>
2025-09-20 17:08:26 +0200

    feat: disable spa mode and add serverOnly from tanstack start

--- vite.config.ts
+++ vite.config.ts
@@ -36,7 +36,7 @@ export default defineConfig({
         routeToken: "layout",
       },
       spa: {
-        enabled: true,
+        enabled: false,
       },
       customViteReactPlugin: true,
     }),
